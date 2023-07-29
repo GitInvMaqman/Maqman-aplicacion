f819aae4b6c463ccae9033c675f94ed2777f855e
GitInvMaqman <correo.inv.maqman@gmail.com>
2023-07-29 03:07:51 -0400

    Update urls.py

--- MaqmanSite/urls.py
+++ MaqmanSite/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('Reportes.urls'))
 ]
-if settings.DEBUG:
+if settings.DEBUG == False:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
