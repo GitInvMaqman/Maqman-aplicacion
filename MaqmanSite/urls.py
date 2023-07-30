ea9c8d7583ac3b73597f8f2eb3f05c3593f594db
GitInvMaqman <correo.inv.maqman@gmail.com>
2023-07-30 04:13:34 -0400

    Update urls.py

--- MaqmanSite/urls.py
+++ MaqmanSite/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
 # if settings.DEBUG == False:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
 # if settings.DEBUG:
 #     urlpatterns += [
