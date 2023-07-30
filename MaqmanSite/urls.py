57074266eda117206ff5e1098d6025589cbd5e0d
GitInvMaqman <correo.inv.maqman@gmail.com>
2023-07-30 03:59:52 -0400

    urls.py

--- MaqmanSite/urls.py
+++ MaqmanSite/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import re_path
-from django.views.static import serve
+# from django.urls import re_path
+# from django.views.static import serve
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -26,14 +26,17 @@ urlpatterns = [
 ]
 # if settings.DEBUG == False:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
 if settings.DEBUG:
-    urlpatterns += [
-        re_path(
-            r"^media/(?P<path>.*)$",
-            serve,
-            {
-                "document_root": settings.MEDIA_ROOT,
-            },
-        ),
-    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    
+# if settings.DEBUG:
+#     urlpatterns += [
+#         url(
+#             r"^media/(?P<path>.*)$",
+#             serve,
+#             {
+#                 "document_root": settings.MEDIA_ROOT,
+#             },
+#         ),
+#     ]
